8e326919d3dac4fb54d0d4e43703c642652cbb69
Saturn corgi <tylerclayton183@gmail.com>
2024-12-19 15:50:04 -0800

    now it's actually just drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -65,6 +65,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return manipulatorCommands.setSpeed(()->1);
   }
 }
